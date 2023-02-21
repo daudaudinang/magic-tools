5df4b0ac90588f00dc87ddb82800707a25fd66ac
huynq <huynguyenquang@vccorp.vn>
2023-02-21 16:33:30 +0700

    Update version

--- src/Log.ts
+++ src/Log.ts
@@ -1,3 +1,12 @@
+import { EventEmitterClass } from "./EventEmitter";
+
+const privateEvents = {
+  "INFO": "INFO", // Thông tin liên quan đến cuộc họp
+  "WARNING": "WARNING", // Cảnh báo
+  "ERROR": "ERROR", // Lỗi
+  "SYSTEM": "SYSTEM", // Thông báo của webapp
+}
+
 const TYPE = {
   "INFO": "INFO", // Thông tin liên quan đến cuộc họp
   "WARNING": "WARNING", // Cảnh báo
@@ -14,10 +23,17 @@ const handleSystemTime = () => {
   return `[${hour < 10 ? `0${hour}` : hour}:${minute < 10 ? `0${minute}` : minute}:${second < 10 ? `0${second}` : second}]`;
 }
 
-const side = 'CLIENT'; // or 'SDK'
+class Logger {
+  privateEventEmitter:EventEmitterClass;
+  side:string = 'SDK';
 
-class LOG {
-  static log = (type:string, event: string, ...args: any[]) => {
+  constructor(side:string) {
+    this.privateEventEmitter = new EventEmitterClass();
+    this.side = side;
+  }
+
+  private log = (type:string, event: string, ...args: any[]) => {
+    const side = this.side.toUpperCase();
     if(typeof type === "string") type = type?.toUpperCase();
     if(type === "event") event = event.toUpperCase();
   
@@ -26,44 +42,55 @@ class LOG {
     switch(type) {
       case TYPE.ERROR: {
         const style = 'color: red;';
-        console.log(`[${time}]%c[${side}][${type}][${event}]:`, style, ...args);
+        console.log(`[${time}]%c[${this.side}][${type}][${event}]:`, style, ...args);
+        this.privateEventEmitter.dispatch(privateEvents.INFO, {time, side, type, event});
         break;
       }
       case TYPE.WARNING: {
         const style = 'color: orange;';
         console.log(`[${time}]%c[${side}][${type}][${event}]:`, style, ...args);
+        this.privateEventEmitter.dispatch(privateEvents.WARNING, {time, side, type, event});
         break;
       }
       case TYPE.INFO: {
         const style = 'color: green;';
         console.log(`[${time}]%c[${side}][${type}][${event}]:`, style, ...args);
+        this.privateEventEmitter.dispatch(privateEvents.ERROR, {time, side, type, event});
         break;
       }
       case TYPE.SYSTEM: {
         const style = 'color: violet;';
         console.log(`[${time}]%c[${side}][${type}][${event}]:`, style, ...args);
+        this.privateEventEmitter.dispatch(privateEvents.SYSTEM, {time, side, type, event});
         break;
       }
     }
   }
-  
-  static error = (event: string, ...args: any[]) => {
-    return LOG.log(TYPE.ERROR, event, ...args);
+
+  public changeSide(side: string) {
+    this.side = side;
   }
   
-  static info = (event: string, ...args: any[]) => {
-    return LOG.log(TYPE.INFO, event, ...args);
+  public error = (event: string, ...args: any[]) => {
+    return this.log(TYPE.ERROR, event, ...args);
   }
   
-  static warning = (event: string, ...args: any[]) => {
-    return LOG.log(TYPE.WARNING, event, ...args);
+  public info = (event: string, ...args: any[]) => {
+    return this.log(TYPE.INFO, event, ...args);
   }
   
-  static system = (event: string, ...args: any[]) => {
-    return LOG.log(TYPE.SYSTEM, event, ...args);
+  public warning = (event: string, ...args: any[]) => {
+    return this.log(TYPE.WARNING, event, ...args);
+  }
+  
+  public system = (event: string, ...args: any[]) => {
+    return this.log(TYPE.SYSTEM, event, ...args);
   }
 }
 
+const Log = new Logger('SDK');
+
 export {
-  LOG
+  Log,
+  Logger
 }
